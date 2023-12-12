18394b0472223db42605677981eff494029876b3
DanAle3 <dannelb.aleman@gmail.com>
2023-12-11 16:51:53 -0800

    updates to structure, and created main.js file.

--- frontend/main.js
+++ frontend/main.js
@@ -1,16 +1,15 @@
 window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
-
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApi = '';
 
 const getVisitCount = () => {
     let count = 30;
     fetch(functionApi).then(response => {
         return response.json()
     }).then(response =>{
-        console.log("Website called function API.")
-        count = response.count;
+        console.log("Website called function API.");
+        count =  response.count;
         document.getElementById("counter").innerText = count;
     }).catch(function(error){
         console.log(error);
